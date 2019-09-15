d606d939c8b682ba06c45770cba81ae917158b16
Jonesc30 <46267335+Jonesc30@users.noreply.github.com>
2019-09-14 19:59:38 -0700

    Add files via upload

--- DesignAssignments/DA1A/SimpleASM/SimpleASM/main.asm
+++ DesignAssignments/DA1A/SimpleASM/SimpleASM/main.asm
@@ -7,10 +7,11 @@
 
 
 .org 0			;burn into ROM starting at 0
-ldi R25, 0x30	;load upper 8 bits of multiplicand
-ldi R24, 0x30	;load lower 8 bits of multiplicand
-ldi R23, 0x30	;load upper 8 bits of multiplier
-ldi R22, 0x30	;load lower 8 bits of multiplier
+ldi R25, 0xFF	;load upper 8 bits of multiplicand
+ldi R24, 0xFF	;load lower 8 bits of multiplicand
+ldi R23, 0xFF	;load upper 8 bits of multiplier
+ldi R22, 0xFF	;load lower 8 bits of multiplier
+ldi R17, 0		;initialize to zero
 ldi R18, 0		;initialize to zero
 ldi R19, 0		;initialize to zero
 ldi R20, 0		;initialize to zero
@@ -19,16 +20,16 @@ ldi R21, 0		;used to carry into R20
 L1:
 add R17, R24	;add lower 8 bits
 adc R18, R25	;add upper 9 bits
-adc R20, R21	;add carry
+adc R19, R20	;add carry
 dec R23			; decrement counter of upper 8 bits of multiplier
 brne L1			; loops while counter is greater than 0
 
 L2:
-add R17, R24	;add lower 8 bits
-adc R18, R25	;add upper 9 bits
+add R18, R24	;add lower 8 bits
+adc R19, R25	;add upper 9 bits
 adc R20, R21	;add carry
 dec R22			; decrement counter of lower 8 bits of multiplier
-brne L1			; loops while counter is greater than 0
+brne L2			; loops while counter is greater than 0
 
 END:
 jmp END			;ends program
